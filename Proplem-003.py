589b197f6190a60cead3d8306d2cfd39a1ff7a0c
Ozan <barca-ozan@hotmail.com>
2023-07-02 22:16:04 +0300

    Projecteuler solutions

---
+++ Proplem-003.py
@@ -0,0 +1,20 @@
+import math
+
+def is_prime(sayı):
+    if sayı <= 1:
+        return False
+    for i in range(2,int(sayı**0.5)+1):
+        if sayı % i == 0:
+            return False
+    return True
+
+number = 600851475143
+largest_pri_fac = 0
+for i in range(2,int(math.sqrt(number))+1):
+    if number % i == 0:
+        if is_prime(i):
+            largest_pri_fac = i
+
+print(largest_pri_fac)
+
+# Output: 6857
